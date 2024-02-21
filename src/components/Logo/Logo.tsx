2101aa4e1bc86a40dc2a6fb4938bfcb33b894cdc
ArthurRocha2100 <arthurrocha2101@gmail.com>
2024-02-21 19:03:27 -0300

    Melhoria em componentes TextLink e Ajuste Index.css

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,12 +2,13 @@ import styles from './Logo.module.css'
 import LogoVetor from '../../img/logo.png'
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../../routes'
+import { LogoPros } from './Logo.props'
 
-export const Logo = ()=> {
+export const Logo:React.FC<LogoPros> = ({secondary})=> {
     return (
         <Link className={styles.Logo} to={ROUTES.Home}>
             <img className={styles.LogoVetor} src={LogoVetor} alt='Vetor da logo' />
-            <p className={styles.LogoTitle}>Digital College</p>
+            <p className={`${styles.LogoTitle}  ${ secondary ? styles.Secondary : ''}`}>Digital College</p>
         </Link>
     )
 }
