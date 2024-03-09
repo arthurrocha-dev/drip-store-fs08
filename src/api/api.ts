fe2f4db542c9fcb7dfa190aaa0ac201db5ea0a3d
ArthurRocha2100 <arthurrocha2101@gmail.com>
2024-03-09 19:20:16 -0300

    Implementando Axios

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,16 @@
+import axios from 'axios'
+
 import { SalesResult, ProductsListResult } from './api.props'
 import { FEATURED_PRODUCTS_URL, SALES_URL } from './api.urls'
 
 export const getProducts = async (): Promise<ProductsListResult[]> => {
-  return await fetch(FEATURED_PRODUCTS_URL).then(
-    async (response) => await response.json()
-  )
+  return await axios
+    .get(FEATURED_PRODUCTS_URL)
+    .then(async (response) => await response.data)
 }
 
 export const getSales = async (): Promise<SalesResult[]> => {
-  return await fetch(SALES_URL).then(async (response) => await response.json())
+  return await axios
+    .get(SALES_URL)
+    .then(async (response) => await response.data)
 }
