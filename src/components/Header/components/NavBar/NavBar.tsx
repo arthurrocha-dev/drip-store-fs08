5ce8d2da711d3ac06ecdde891a59ba76d880fc56
ArthurRocha2100 <arthurrocha2101@gmail.com>
2024-03-09 18:33:50 -0300

    Corrigindo scroll na troca de página

--- src/components/Header/components/NavBar/NavBar.tsx
+++ src/components/Header/components/NavBar/NavBar.tsx
@@ -8,16 +8,40 @@ export const NavBar: React.FC<NavBarProps> = ({ onClick }) => {
     <>
       <p className={styles.NavBarTitle}>Páginas</p>
       <div className={styles.NavBar}>
-        <TextLink to={ROUTES.Home} onClick={onClick}>
+        <TextLink
+          to={ROUTES.Home}
+          onClick={() => {
+            onClick
+            window.scroll(0, 0)
+          }}
+        >
           Home
         </TextLink>
-        <TextLink to={ROUTES.Products} onClick={onClick}>
+        <TextLink
+          to={ROUTES.Products}
+          onClick={() => {
+            onClick
+            window.scroll(0, 0)
+          }}
+        >
           Produtos
         </TextLink>
-        <TextLink to={ROUTES.Categorys} onClick={onClick}>
+        <TextLink
+          to={ROUTES.Categorys}
+          onClick={() => {
+            onClick
+            window.scroll(0, 0)
+          }}
+        >
           Categorias
         </TextLink>
-        <TextLink to={ROUTES.MyProducts} onClick={onClick}>
+        <TextLink
+          to={ROUTES.MyProducts}
+          onClick={() => {
+            onClick
+            window.scroll(0, 0)
+          }}
+        >
           Meus Produtos
         </TextLink>
       </div>
