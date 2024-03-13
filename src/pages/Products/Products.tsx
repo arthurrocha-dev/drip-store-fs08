fbb49b788e143d19ce30d2e9e1f692a46d7c8d43
ArthurRocha2100 <arthurrocha2101@gmail.com>
2024-03-13 00:26:59 -0300

    implementação inicial do componente de ordenação

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,11 +1,25 @@
-import { ProductsFilters, ProductsList } from '../../components'
+import { Dropdown, ProductsFilters, ProductsList } from '../../components'
 import styles from './Products.module.css'
 
 export const Products = () => {
   return (
-    <div className={styles.ProductsContainer}>
-      <ProductsFilters />
-      <ProductsList/>
-    </div>
+    <>
+      <div className={styles.ProductsHeader}>
+        <p>Total de resultados</p>
+        <div className={styles.ProductsListOrdernation}>
+          <p>Ordenar por:</p>
+          <Dropdown name="Ordenar por:" dropdownId="Dropdown1">
+            <option value="price">Valor</option>
+            <option value="description">Descrição</option>
+            <option value="relevante">Relevância</option>
+          </Dropdown>
+        </div>
+      </div>
+
+      <div className={styles.ProductsContainer}>
+        <ProductsFilters />
+        <ProductsList />
+      </div>
+    </>
   )
 }
