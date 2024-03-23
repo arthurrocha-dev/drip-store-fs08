a395f8d1307fb9d57d678f93c8f300b276307e21
ArthurRocha2100 <arthurrocha2101@gmail.com>
2024-03-23 01:22:29 -0300

    Ajuste de diretorio de imagens

--- src/components/FeaturedCollections/FeaturedCollections.tsx
+++ src/components/FeaturedCollections/FeaturedCollections.tsx
@@ -8,9 +8,9 @@ export const FeaturesCollections = (): JSX.Element => {
       <div className={styles.CollectionsContainer}>
         <p className={styles.Title}>{"Coleções em destaque"}</p>
         <div className={styles.CardsList}>
-          <CollectionCard discount={40} title='Novo Drop Supreme'urlImg="src/components/FeaturedCollections/components/CollectionCard/img/tShirtCollection.png"/>
-          <CollectionCard discount={25} title='Coleção Adidas' urlImg="src/components/FeaturedCollections/components/CollectionCard/img/tennisCollection.png"/>
-          <CollectionCard discount={15} title='Novo Beats Bass' urlImg="src/components/FeaturedCollections/components/CollectionCard/img/headPhoneCollection.png"/>
+          <CollectionCard discount={40} title='Novo Drop Supreme'urlImg="img/tShirtCollection.png"/>
+          <CollectionCard discount={25} title='Coleção Adidas' urlImg="img/tennisCollection.png"/>
+          <CollectionCard discount={15} title='Novo Beats Bass' urlImg="img/headPhoneCollection.png"/>
         </div>
 
         <p className={styles.SupTitle}>Categorias em destaque</p>
