32657fb9d4726795dee723707e27d8b54e2f3353
Luis Miguel <lmigueldev@gmail.com>
2024-03-22 21:59:14 -0300

    update url

--- src/api/api.urls.ts
+++ src/api/api.urls.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = 'https://drip-store-fs08-temp-api.vercel.app'
 
 export const FEATURED_PRODUCTS_URL = `${BASE_URL}/products`
 export const SALES_URL = `${BASE_URL}/sales`
